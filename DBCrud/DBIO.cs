c6af63383f2bc87cb050e4700226c22761a55e0b
Trần Thành Lộc <loc.tran@ntq-solution.com.vn>
2022-02-10 16:18:23 +0700

    Dat Commit CRUD Oracle with EF

---
+++ DBCrud/DBIO.cs
@@ -0,0 +1,71 @@
+﻿using DbFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBCrud
+{
+    /// <summary>
+    /// Code by Dat
+    /// </summary>
+    public class DBIO
+    {
+        OracleDb _context = new OracleDb();
+        public IQueryable<PERSON> GetAll()
+        {
+            return _context.People.AsQueryable();
+        }
+        public IQueryable<PERSON> GetAllWithCondional(Expression<Func<PERSON,bool>> expression)
+        {
+            return _context.People.Where(expression).OrderBy(x=> x.FULLNAME).ThenBy(i => i.ID).AsQueryable();
+        }
+        public PERSON GetById(string id)
+        {
+            return _context.People.Where(x => x.ID == id).FirstOrDefault();
+        }
+        public void Insert(PERSON person)
+        {
+            try
+            {
+                _context.People.Add(person);
+                _context.SaveChanges();
+                Console.WriteLine("Add success!");
+
+            }
+            catch
+            {
+                Console.WriteLine("ID was exist!");
+            }
+        }
+        public void Update(PERSON person)
+        {
+            try
+            {
+                _context.Entry(person).State = EntityState.Modified;
+                _context.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        public void Delete(string id)
+        {
+            var person = GetById(id);
+            if(person == null)
+            {
+                Console.WriteLine("Can not find person");
+            }
+            else
+            {
+                _context.People.Remove(person);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DBCrud/Program.cs
@@ -0,0 +1,48 @@
+﻿using DbFirst.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBCrud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DBIO db = new DBIO();
+            PERSON person = new PERSON
+            {
+                ID = "DAT",
+                FULLNAME = "Peter Par",
+                DOB = new DateTime(1990, 4, 5),
+                DEPARTMENT = "DPA",
+                NOTE = "ABC",
+                SALARY = 3434
+            };
+            //db.Insert(person);
+
+            //db.Update(person);
+
+            IQueryable people = db.GetAll();
+            foreach(PERSON i in people)
+            {
+                Console.WriteLine($"{i.ID}, {i.FULLNAME}, {i.DOB}, {i.DEPARTMENT}, {i.SALARY}, {i.NOTE} ");
+            }
+            //db.Delete("DAT");
+            Console.WriteLine("//////After delete");
+            foreach (PERSON i in people)
+            {
+                Console.WriteLine($"{i.ID}, {i.FULLNAME}, {i.DOB}, {i.DEPARTMENT}, {i.SALARY}, {i.NOTE} ");
+            }
+            Console.WriteLine("////// salary > 10000");
+            IQueryable people2 = db.GetAllWithCondional(x => x.SALARY > 10000);
+            foreach (PERSON i in people2)
+            {
+                Console.WriteLine($"{i.ID}, {i.FULLNAME}, {i.DOB}, {i.DEPARTMENT}, {i.SALARY}, {i.NOTE} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
